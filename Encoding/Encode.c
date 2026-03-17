90c8b5a6b412b96b515b547eca0b0d2f686b64a2
LucaMarks <lucamarks416@gmail.com>
2026-03-16 20:44:26 -0400

    using a 2d array to build huffman tree lol

--- Encoding/Encode.c
+++ Encoding/Encode.c
@@ -20,20 +20,27 @@ void treeRunner(CharacterCount *characterCount, char word[], int size) {
 void makeTree(CharacterCount *characterCount, char word[], int size) {
     //build a huffman tree assuming the characterCount array is already sorted
     //build from the bottom right up
-    int prevLetterCount = 1;
     int currRow = 0;
+    int rowSize = 0;
 
-    //maybe need an array of nodes, should look at how java does trees
+    //add a 2d array of nodes where the list represents an array of rows
+    //  -> Or this is done by priority queue which is probably a better approach
+    //  -> Or just refactor this later
+    treeNode **treeArray = malloc((sizeof(treeNode) * characterCount[size-1].count) * size);
     treeNode *node = createNode(characterCount[0]);
     printf("adding node to tree -> [%c, %d]\n", node[0].cc.letter, node[0].cc.count);
     for (int i = 1; i < size; i++) {
         //make row
-        if (node[i].cc.count > prevLetterCount) {
-            prevLetterCount = node[i].cc.count;
-            currRow++;
-            //need to add up values
-        } else if (node[i].cc.count == prevLetterCount) {
+        if (node->cc.count > currRow+1) {//node[i] might not be the correct thing to do, it's not an array
+            currRow = node[i].cc.count -1;
+            rowSize = 0;
+            //need to add up values from previous row
+
+        } else if (node[i].cc.count == currRow -1) {// add one to the same row
             treeNode *newLeftNode = createNode(characterCount[i]);
+            treeArray[currRow][rowSize] = *newLeftNode;
+
+            rowSize++;
         }
         printf("adding node to tree -> [%c, %d]\n", node[i].cc.letter, node[i].cc.count);
     }
---
+++ cmake-build-debug/CMakeFiles/TextCompressor.dir/DynamicArray/DynamicCharacterCount.c.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/TextCompressor.dir/DynamicArray/DynamicCharacterCount.c.obj: \
+ C:\Users\lucst\Documents\C\ Projects\TextCompressor\DynamicArray\DynamicCharacterCount.c \
+ C:\Users\lucst\Documents\C\ Projects\TextCompressor\DynamicArray\DynamicCharacterCount.h
