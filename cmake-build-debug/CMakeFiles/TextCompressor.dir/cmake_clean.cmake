a4f7f6565236e83901695f798f974ec45e2d6ed1
LucaMarks <lucamarks416@gmail.com>
2026-03-28 02:35:09 -0400

    Edit run config

---
+++ cmake-build-debug/CMakeFiles/TextCompressor.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TextCompressor.dir/Compress/CompressWord.c.obj"
+  "CMakeFiles/TextCompressor.dir/Compress/CompressWord.c.obj.d"
+  "CMakeFiles/TextCompressor.dir/DynamicArray/DynamicCharacterCount.c.obj"
+  "CMakeFiles/TextCompressor.dir/DynamicArray/DynamicCharacterCount.c.obj.d"
+  "CMakeFiles/TextCompressor.dir/Encoding/Encode.c.obj"
+  "CMakeFiles/TextCompressor.dir/Encoding/Encode.c.obj.d"
+  "CMakeFiles/TextCompressor.dir/Encoding/Tree.c.obj"
+  "CMakeFiles/TextCompressor.dir/Encoding/Tree.c.obj.d"
+  "CMakeFiles/TextCompressor.dir/FrequencyAnalysis/Frequency.c.obj"
+  "CMakeFiles/TextCompressor.dir/FrequencyAnalysis/Frequency.c.obj.d"
+  "CMakeFiles/TextCompressor.dir/main.c.obj"
+  "CMakeFiles/TextCompressor.dir/main.c.obj.d"
+  "TextCompressor.exe"
+  "TextCompressor.exe.manifest"
+  "TextCompressor.pdb"
+  "libTextCompressor.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TextCompressor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
