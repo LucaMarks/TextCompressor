ea152810e4cb0597a596c0237214343a374002f0
LucaMarks <lucamarks416@gmail.com>
2026-03-27 20:14:36 -0400

    Fixed issue with node setting. Was using the wrong index value

--- Encoding/Encode.c
+++ Encoding/Encode.c
@@ -7,35 +7,51 @@ void makeTree(CharacterCount *characterCount, char word[], int size);
 TreeNode *getLeafNodes(CharacterCount *characterCount, int size);
 void cleanUpArray(TreeNode **nodes, int size);
 void printNodes(TreeNode *nodes, int size);
+void connectToHead(TreeNode *nodes, int size);
 
 void treeRunner(CharacterCount *characterCount, char word[], int size) {
+    printf("\n");
     makeTree(characterCount, word, size);
 }
 
 //build huffman tree
 //every item in characterCount is a leaf node
 void makeTree(CharacterCount *characterCount, char word[], int size) {
+    // printf("Size is %d", size);
     TreeNode *leafs = getLeafNodes(characterCount, size);
     //add up all leaf nodes in this order
     TreeNode *nodes = malloc(size * sizeof(TreeNode));
     int nodeSize = 0;
     // printf("leaf node -> [%c %d]\n", leafs[0].letter, leafs[0].level);
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i+=2) {
         // printf("Made it here %d\n", i);
         // printf("leaf node -> [%c %d]\n", leafs[i].letter, leafs[i].level);
         TreeNode *node = createNode(leafs[i].level + leafs[i+1].level);
         setRight(node, &leafs[i]);
         setLeft(node, &leafs[i+1]);
-        nodes[i] = *node;
+        nodes[nodeSize] = *node;
         // printf("working node -> [%d]\n", nodes[i].level);
-        i++;
+        // i++;
         nodeSize++;
     }
     //issue with cleanUpArray
-    cleanUpArray(nodes, nodeSize);
+    cleanUpArray(&nodes, nodeSize);
+    //print nodes has a runtime error
     printNodes(nodes, nodeSize);
-
+    connectToHead(nodes, nodeSize);
 }
+// void connectToHead(TreeNode *nodes, int size) {
+//     //do the same thing as before
+//     TreeNode *nextLevelNodes = malloc(size * sizeof(TreeNode));
+//     int nodeSize = 0;
+//     for (int i = 0; i < size; i+=2) {
+//         TreeNode *node = createNode(nodes[i].level + nodes[i+1].level);
+//         nextLevelNodes[i] = *node;
+//         printf("")
+//         nodeSize++;
+//     }
+// }
+
 TreeNode *getLeafNodes(CharacterCount *characterCount, int size) {
     TreeNode *nodes = malloc(size * sizeof(TreeNode));
     for (int i = 0; i < size; i++) {
@@ -48,7 +64,10 @@ TreeNode *getLeafNodes(CharacterCount *characterCount, int size) {
 void cleanUpArray(TreeNode **nodes, int size) {
     TreeNode *newNodes = malloc(size * sizeof(TreeNode));
     for (int i = 0; i < size; i++) {
-        newNodes[i] = *nodes[i];
+        newNodes[i] = (*nodes)[i];
+        // printf("%d ", newNodes[i].level);
+        // printf("%d, [%c %d]\n", i, nodes[i]->letter, nodes[i]->level);
+        // printf("%d, [%c %d] <- [%c %d] -> [%c %d]\n", i, nodes[i]->left->letter, nodes[i]->left->level, 'z', nodes[i]->level, nodes[i]->right->letter, nodes[i]->right->level);
     }
     free(*nodes);
     *nodes = newNodes;
@@ -57,6 +76,6 @@ void cleanUpArray(TreeNode **nodes, int size) {
 //will need to refactor this to handle more cases
 void printNodes(TreeNode *nodes, int size) {
     for (int i = 0; i < size; i++) {
-        printf("[%c %d] <- [%c %d] -> [%c %d]\n", nodes[i].left->letter, nodes[i].left->level, nodes[i].letter, nodes[i].level, nodes[i].right->letter, nodes[i].right->level);
+        printf("%d, [%c %d] <- [%c %d] -> [%c %d]\n", i, nodes[i].left->letter, nodes[i].left->level, 'z', nodes[i].level, nodes[i].right->letter, nodes[i].right->level);
     }
 }
