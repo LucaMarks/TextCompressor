c785cc3fa13d093a575f7090a648f16ba5dd9369
LucaMarks <lucamarks416@gmail.com>
2026-03-29 05:53:13 -0400

    config fix

--- cmake-build-debug/CMakeFiles/TextCompressor.dir/DynamicArray/DynamicCharacterCount.c.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/TextCompressor.dir/DynamicArray/DynamicCharacterCount.c.obj: \
- C:\Users\lucst\Documents\CProjects\TextCompressor\DynamicArray\DynamicCharacterCount.c \
- C:\Users\lucst\Documents\CProjects\TextCompressor\DynamicArray\DynamicCharacterCount.h
